15fd2064d90d7f0bfdbd531595f34995b00bd975
axel39120-gif <axel39120@gmail.com>
2026-07-20 20:55:33 +0200

    Add files via upload

--- Racing Journey 2.0/service-worker.js
+++ Racing Journey 2.0/service-worker.js
@@ -1,5 +1,5 @@
 // Bump this version on every deploy to force old caches to be purged.
-const CACHE_NAME = 'racing-journey-pwa-v50';
+const CACHE_NAME = 'racing-journey-pwa-v51';
 
 const urlsToCache = [
   './',
